16e6a155bd7cb04dcf90b21d9a7bf36a3eb6f53b
Aria <2012sko@gmail.com>
2020-09-11 14:47:21 +0200

    Even smaller!!!

--- smallestmissing.cpp
+++ smallestmissing.cpp
@@ -6,11 +6,11 @@ int findSmallestMissing(std::vector<int> numbers, int min) {
     int iterate = 0;
     int candidate = min + 1;
     while(iterate < numbers.size()) {
-        if(numbers.at(iterate) == candidate) {
+        if(numbers.at(iterate) == candidate) { //if the following number is found, increment the candidate and reset the iterator
             candidate++;
             iterate = 0;
         }
-        else {
+        else { //iterate until if statement is true, or until while loop completes
             iterate++;
         }
     }
@@ -18,7 +18,6 @@ int findSmallestMissing(std::vector<int> numbers, int min) {
 }
 
 int main() {
-    int smallestMissing;
     int amountNums;
     int nextNum;
     int minimum;
@@ -27,8 +26,8 @@ int main() {
     std::cin >> amountNums;
     std::vector<int> numbers;
     std::cout << std::endl << "Please enter the numbers ";
-    std::cin >> nextNum;
 
+    std::cin >> nextNum; // populate vector
     numbers.push_back(nextNum);
     for(int i = 0; i < amountNums - 1; i++) {
         std::cin >> nextNum;
