9c3ddd3251e5c57dd5e5232e46c271b0bd4effa7
Aria <2012sko@gmail.com>
2020-09-18 13:53:55 +0200

    Added error-handling to Polish

--- polish.cpp
+++ polish.cpp
@@ -20,29 +20,39 @@ int main() {
     double second;
 
     while (oper != 'q') {
-        std::cout << "Enter the expression:";
-        std::cin >> oper;
-        if(oper == 'q') {
-            break;
-        }
-        std::cin >> first >> second;
+        try {
+            std::cout << "Enter the expression:";
+            std::cin >> oper;
+            if(oper == 'q') {
+                throw(std::runtime_error("User exit"));
+            }
+            std::cin >> first >> second;
+            if(std::cin.fail()) {
+                std::cin.clear();
+                throw std::runtime_error("Invalid number input");
+            }
 
-        switch (oper) {
-            case '+':
-                std::cout << first << " " << oper << " " << second << " = " << add(first,second) << std::endl;
-                break;
-            case '-':
-                std::cout << first << " " << oper << " " << second << " = " << subtract(first,second) << std::endl;
-                break;
-            case '*':
-                std::cout << first << " " << oper << " " << second << " = " << multiply(first,second) << std::endl;
-                break;
-            case '/':
-                std::cout << first << " " << oper << " " << second << " = " << divide(first,second) << std::endl;
-                break;
-            default:
-                std::cout << "The operator " << oper << " does not exist. Please try again. " << std::endl;
-                break;
+            switch (oper) {
+                case '+':
+                    std::cout << first << " " << oper << " " << second << " = " << add(first,second) << std::endl;
+                    break;
+                case '-':
+                    std::cout << first << " " << oper << " " << second << " = " << subtract(first,second) << std::endl;
+                    break;
+                case '*':
+                    std::cout << first << " " << oper << " " << second << " = " << multiply(first,second) << std::endl;
+                    break;
+                case '/':
+                    std::cout << first << " " << oper << " " << second << " = " << divide(first,second) << std::endl;
+                    break;
+                default:
+                    throw std::runtime_error("Invalid operator");
+                    break;
+        
+            }
+        }
+        catch (std::runtime_error& excpt) {
+            std::cout << "An error occurred: " << excpt.what() << std::endl;
         }
     }
     return 0;   
